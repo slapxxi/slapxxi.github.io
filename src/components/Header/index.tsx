2bdb212a90360e47e02e8e8da63be396b42b85c7
Slava Pavlutin <sl.pavlutin@gmail.com>
2026-07-04 11:44:41 +0300

    tweaks

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,14 +3,14 @@ import config from './config'
 export const Header = () => {
   return (
     <header className="col-[content] py-4">
-      <ul className="flex gap-4 flex-wrap">
+      <ul className="flex gap-3 flex-wrap items-center justify-center">
         {config.links.map((link) => (
           <li key={link.name}>
             <a
               href={link.repo}
               rel="noreferrer"
               target="_blank"
-              className="flex items-center gap-2 text-zinc-950/90 hover:text-blue-800/80 text-sm"
+              className="flex items-center gap-1 text-zinc-950/90 hover:text-blue-800/80 text-xs md:text-lg"
             >
               <link.Icon className="size-6" />
               <span>{link.name}</span>
